36e1846c967d0835966959acd157413c0c969c78
GASEN1216 <GASEN1216@163.com>
2024-03-10 23:48:28 +0800

    excel导入数据domain类

---
+++ src/main/java/com/gasen/findmeetbackend/once/importuser/importUser.java
@@ -0,0 +1,16 @@
+package com.gasen.findmeetbackend.once.importuser;
+
+import lombok.Data;
+
+/**
+ * excel导入用户类
+ * @author GASEN
+ * @date 2024/3/9 19:01
+ * @classType description
+ */
+@Data
+public class importUser {
+
+    private String userAccount;
+    private String password;
+}
