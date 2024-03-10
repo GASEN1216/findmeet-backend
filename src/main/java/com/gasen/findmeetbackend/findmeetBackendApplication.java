9958e139b699958cbb80a91f1b6bd15d8b35fc90
GASEN1216 <GASEN1216@163.com>
2024-03-10 23:39:13 +0800

    添加定时任务，分布式session注解

--- src/main/java/com/gasen/findmeetbackend/findmeetBackendApplication.java
+++ src/main/java/com/gasen/findmeetbackend/findmeetBackendApplication.java
@@ -3,9 +3,13 @@ package com.gasen.findmeetbackend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @MapperScan("com.gasen.findmeetbackend.mapper")
+@EnableScheduling
+@EnableRedisHttpSession
 public class findmeetBackendApplication {
 
     public static void main(String[] args) {
