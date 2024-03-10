7abe9056016c7fa42aba12ed29cedba96d0c1aaa
GASEN1216 <GASEN1216@163.com>
2024-03-10 23:49:09 +0800

    excel插入数据类

---
+++ src/main/java/com/gasen/findmeetbackend/once/importuser/InsertUsers.java
@@ -0,0 +1,55 @@
+package com.gasen.findmeetbackend.once.importuser;
+import java.time.LocalDateTime;
+
+import com.alibaba.excel.EasyExcel;
+import com.gasen.findmeetbackend.mapper.UserMapper;
+import com.gasen.findmeetbackend.model.User;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+/**
+ * 插入用户
+ * @author GASEN
+ * @date 2024/3/10 10:15
+ * @classType description
+ */
+@Component
+public class InsertUsers {
+
+    @Resource
+    private importUserListener importUserListener;
+
+    @Resource
+    private UserMapper userMapper;
+
+    //@Scheduled(initialDelay = 5000, fixedRate = Long.MAX_VALUE)
+    public void insertusers() {
+        String filePath = "C:\\Users\\Administrator\\Desktop\\MOCK_DATA.xlsx";
+        EasyExcel.read(filePath, importUser.class, importUserListener).sheet().doRead();
+    }
+
+    //@Scheduled(initialDelay = 0, fixedRate = Long.MAX_VALUE)
+    public void doInsertUser() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        final int INSERT_NUM = 1000;
+        for(int i = 0; i < INSERT_NUM; i++) {
+            User user = new User();
+            user.setUserName("gasen");
+            user.setUserAccount("gasen");
+            user.setPassword("12345678");
+            user.setAvatarUrl("mypicture.com");
+            user.setGender(0);
+            user.setEmail("gasen12345@163.com");
+            user.setPhone("112354685");
+            user.setTags("[]");
+            userMapper.insert(user);
+        }
+        stopWatch.stop();
+        System.out.println("插入用户耗时：" + stopWatch.getTotalTimeSeconds() + "s");
+    }
+}
---
+++ src/main/java/com/gasen/findmeetbackend/once/importuser/importUserListener.java
@@ -0,0 +1,61 @@
+package com.gasen.findmeetbackend.once.importuser;
+
+import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.read.listener.ReadListener;
+import com.alibaba.excel.util.ListUtils;
+
+import com.gasen.findmeetbackend.controller.UserController;
+import com.gasen.findmeetbackend.mapper.UserMapper;
+import com.gasen.findmeetbackend.model.User;
+import com.gasen.findmeetbackend.service.IUserService;
+import com.gasen.findmeetbackend.service.impl.UserServiceImpl;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
+
+import java.util.List;
+
+/**
+ * @author GASEN
+ * @date 2024/3/9 19:03
+ * @classType description
+ */
+// 有个很重要的点 importUserListener 不能被spring管理，要每次读取excel都要new,然后里面用到spring可以构造方法传进去
+@Component
+@Slf4j
+public class importUserListener implements ReadListener<importUser> {
+
+    private static final int BATCH_COUNT = 100;
+
+    private List<User> cachedDataList = ListUtils.newArrayListWithExpectedSize(BATCH_COUNT);
+
+    @Resource
+    private IUserService userService;
+
+    @Override
+    public void invoke(importUser data, AnalysisContext context) {
+        cachedDataList.add(new User(data.getUserAccount(), data.getPassword()));
+        // 达到BATCH_COUNT了，需要去存储一次数据库，防止数据几万条数据在内存，容易OOM
+        if (cachedDataList.size() >= BATCH_COUNT) {
+            saveData();
+            // 存储完成清理 list
+            cachedDataList = ListUtils.newArrayListWithExpectedSize(BATCH_COUNT);
+        }
+    }
+
+    @Override
+    public void doAfterAllAnalysed(AnalysisContext context) {
+        // 这里也要保存数据，确保最后遗留的数据也存储到数据库
+        saveData();
+        log.info("所有数据解析完成！");
+    }
+
+    private void saveData() {
+        log.info("{}条数据，开始存储数据库！", cachedDataList.size());
+        cachedDataList.forEach(user -> user.setPassword(DigestUtils.md5DigestAsHex(user.getPassword().getBytes())));
+        userService.saveBatch(cachedDataList);
+        log.info("存储数据库成功！");
+    }
+}
