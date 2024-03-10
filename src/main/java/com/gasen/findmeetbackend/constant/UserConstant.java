4fdf9224351ce393a2f545c84d75aa47aac2c658
GASEN1216 <GASEN1216@163.com>
2024-03-10 23:43:09 +0800

    新增盐值

--- src/main/java/com/gasen/findmeetbackend/constant/UserConstant.java
+++ src/main/java/com/gasen/findmeetbackend/constant/UserConstant.java
@@ -4,6 +4,11 @@ public interface UserConstant {
 
     String USER_LOGIN_IN = "userLoginIn";
 
+    /**
+     * 盐值混淆密码
+     * */
+    String SALT = "20240225";
+
     int ADMIN = 1;
 
     int USER = 0;
