6857a43afd3f125e4be7adf6d3e209ae9bbf344e
GASEN1216 <GASEN1216@163.com>
2024-03-10 23:39:45 +0800

    测试根据标签查询用户接口

--- src/test/java/com/gasen/findmeetbackend/findmeetBackendApplicationTests.java
+++ src/test/java/com/gasen/findmeetbackend/findmeetBackendApplicationTests.java
@@ -1,16 +1,29 @@
 package com.gasen.findmeetbackend;
 
+import com.gasen.findmeetbackend.model.User;
+import com.gasen.findmeetbackend.service.impl.UserServiceImpl;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.List;
+
+
 @SpringBootTest
+@Slf4j
 class findmeetBackendApplicationTests {
 
-
+    @Resource
+    private UserServiceImpl userService;
 
     @Test
     void contextLoads() {
-
+        List<String> tags = Arrays.asList("java", "c++", "Python");
+        log.info(tags.toString());
+        List<User> users = userService.selectByTags(tags);
+        System.out.println(users);
     }
 
 }
