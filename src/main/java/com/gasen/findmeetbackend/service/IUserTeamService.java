f3a3d799e2d047c94c6831d601404e165afafbcf
GASEN1216 <GASEN1216@163.com>
2024-03-14 16:01:34 +0800

    用户队伍表的service层

---
+++ src/main/java/com/gasen/findmeetbackend/service/IUserTeamService.java
@@ -0,0 +1,16 @@
+package com.gasen.findmeetbackend.service;
+
+import com.gasen.findmeetbackend.model.domain.UserTeam;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 用户队伍表 服务类
+ * </p>
+ *
+ * @author gasen
+ * @since 2024-03-13
+ */
+public interface IUserTeamService extends IService<UserTeam> {
+
+}
