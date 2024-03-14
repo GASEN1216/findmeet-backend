f2dbdc7e711aa7b2ab88e08747000bc75ed0e222
GASEN1216 <GASEN1216@163.com>
2024-03-14 16:10:54 +0800

    添加team表，user_team表的sql建表语句

--- sql/user.sql
+++ sql/user.sql
@@ -23,3 +23,32 @@ create table user
 INSERT INTO gasen.user (id, user_name, user_account, password, avatar_url, gender, email, phone, grade, exp, sign_in, state, unblocking_time, is_delete, create_time, update_time) VALUES (1, null, 'test01', 'd02c77b7b10e5349c50d83898cdac269', null, null, null, null, 4, 16, '2024-02-25 23:39:56', 1, null, 0, '2024-02-23 17:12:31', '2024-02-25 23:39:55');
 INSERT INTO gasen.user (id, user_name, user_account, password, avatar_url, gender, email, phone, grade, exp, sign_in, state, unblocking_time, is_delete, create_time, update_time) VALUES (2, '111', 'test02', 'd02c77b7b10e5349c50d83898cdac269', null, null, null, null, 1, 0, '2024-02-25 19:12:59', 0, '2024-02-26 19:12:37', 1, '2024-02-25 18:28:44', '2024-02-25 23:42:25');
 INSERT INTO gasen.user (id, user_name, user_account, password, avatar_url, gender, email, phone, grade, exp, sign_in, state, unblocking_time, is_delete, create_time, update_time) VALUES (3, null, 'test03', 'd02c77b7b10e5349c50d83898cdac269', null, null, null, null, 1, 0, '2024-02-25 23:06:37', 0, null, 1, '2024-02-25 18:30:15', '2024-02-25 23:27:43');
+
+create table team
+(
+    id          int auto_increment comment 'id'
+        primary key,
+    create_user int                                not null comment '创建者id',
+    name        varchar(256)                       null comment '队伍名称',
+    max_num     int      default 1                 not null comment '人员数目',
+    description varchar(1024)                      null comment '描述',
+    state       tinyint  default 0                 not null comment '(0公开，1私有，2加密) ',
+    password    varchar(512)                       null comment '加密密码',
+    is_delete   tinyint  default 0                 not null comment '逻辑删除',
+    create_time datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
+)
+    comment '队伍表';
+
+
+create table user_team
+(
+    id          int auto_increment comment 'id'
+        primary key,
+    user_id     int                                not null comment '用户id',
+    team_id     int                                not null comment '队伍id',
+    is_delete   int      default 0                 not null comment '逻辑删除',
+    create_time datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
+)
+    comment '用户队伍表';
