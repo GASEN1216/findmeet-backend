ab90b32f2557e3f442793bb85aa53f32c84bccee
GASEN1216 <GASEN1216@163.com>
2024-03-14 16:04:34 +0800

    init commit

---
+++ src/main/java/com/gasen/findmeetbackend/mapper/TeamMapper.java
@@ -0,0 +1,16 @@
+package com.gasen.findmeetbackend.mapper;
+
+import com.gasen.findmeetbackend.model.domain.Team;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 队伍表 Mapper 接口
+ * </p>
+ *
+ * @author gasen
+ * @since 2024-03-13
+ */
+public interface TeamMapper extends BaseMapper<Team> {
+
+}
---
+++ src/main/java/com/gasen/findmeetbackend/mapper/UserTeamMapper.java
@@ -0,0 +1,16 @@
+package com.gasen.findmeetbackend.mapper;
+
+import com.gasen.findmeetbackend.model.domain.UserTeam;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户队伍表 Mapper 接口
+ * </p>
+ *
+ * @author gasen
+ * @since 2024-03-13
+ */
+public interface UserTeamMapper extends BaseMapper<UserTeam> {
+
+}
